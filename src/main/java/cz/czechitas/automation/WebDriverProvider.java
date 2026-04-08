003b5a66064139333761045636f81573af421127
brumlablo <barbora.blozonova@gmail.com>
2026-04-08 14:51:19 +0200

    run headless tests on pr and in main

--- src/main/java/cz/czechitas/automation/WebDriverProvider.java
+++ src/main/java/cz/czechitas/automation/WebDriverProvider.java
@@ -5,6 +5,7 @@ import io.github.bonigarcia.wdm.config.WebDriverManagerException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariOptions;
 
 import javax.annotation.Nonnull;
@@ -25,6 +26,10 @@ public final class WebDriverProvider {
         throw new UnsupportedOperationException("Unable to instantiate class with only static methods.");
     }
 
+    private static final boolean HEADLESS = Boolean.parseBoolean(
+            System.getProperty("headless", String.valueOf("true".equalsIgnoreCase(System.getenv("CI"))))
+    );
+
     /**
      * Get new instance of the selenium {@link WebDriver}.
      * The given instance is dependent on web browsers installed on your computer and
@@ -33,12 +38,21 @@ public final class WebDriverProvider {
      *  - Chrome
      *  - Edge
      *
+     * <p>Headless mode is enabled automatically when running in CI (env {@code CI=true})
+     * or when the system property {@code headless=true} is set.</p>
+     *
      * @return web driver instance
      */
     @Nonnull
     public static WebDriver getWebDriver() {
         try {
-            return WebDriverManager.firefoxdriver().create();
+            FirefoxOptions options = new FirefoxOptions();
+            if (HEADLESS) {
+                options.addArguments("--headless");
+                options.addArguments("--width=1920");
+                options.addArguments("--height=1080");
+            }
+            return WebDriverManager.firefoxdriver().capabilities(options).create();
         } catch (WebDriverManagerException exception) {
             return createChromeDriver();
         }
@@ -48,6 +62,12 @@ public final class WebDriverProvider {
         try {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--window-size=1920,1080");
+            if (HEADLESS) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+            }
             return WebDriverManager.chromedriver().capabilities(options).create();
         } catch (WebDriverManagerException exception1) {
             return createEdgeDriver();
@@ -58,6 +78,12 @@ public final class WebDriverProvider {
         try {
             EdgeOptions options = new EdgeOptions();
             options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--window-size=1920,1080");
+            if (HEADLESS) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+            }
             return WebDriverManager.edgedriver().capabilities(options).create();
         } catch (WebDriverManagerException exception1) {
             return createSafariDriver();
